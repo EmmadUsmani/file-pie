de17ce91ef9b4f4d5787055b5f47b655165d60b4
Emmad Usmani <emmadausmani@gmail.com>
2021-09-15 10:55:35 -0700

    feat(file transfer): :sparkles: write skeleton code for webrtc offer/answer exchange in client
    Wrote skeleton code for WebRTC offer/answer exchange in client only (not actually exchanging descriptions yet via server).

--- client/src/download.ts
+++ client/src/download.ts
@@ -1,1 +1,13 @@
-console.log("This is the download script.");
+import { rtcConfig } from "./constants"
+
+const peerConnection = new RTCPeerConnection(rtcConfig)
+// TODO: create dataChannel
+
+// TODO: join socket.io room from link
+void peerConnection.createOffer().then((offer) => handleOffer(offer))
+async function handleOffer(offer: RTCSessionDescriptionInit) {
+  void peerConnection.setRemoteDescription(offer)
+  const answer = await peerConnection.createAnswer()
+  void peerConnection.setLocalDescription(answer)
+  console.log(answer) // TODO: send answer to host in socket.io
+}
--- client/src/index.ts
+++ client/src/index.ts
@@ -1,10 +1,42 @@
-import { sharedFunc, SharedType } from "@webrtc-file-transfer/shared";
+import { rtcConfig } from "./constants"
 
-console.log("This is the index script.");
+const fileInput = document.querySelector<HTMLInputElement>("input#fileInput")
 
-let x: SharedType;
-x = "asdf";
-console.log(x);
-x = 10;
-console.log(x);
-sharedFunc();
+fileInput?.addEventListener("change", handleFileInputChange)
+
+function handleFileInputChange() {
+  if (fileInput && fileInput.files) {
+    console.log("Create Socket.io room in signaling server") // TODO: create room
+    handleReceiverJoin()
+  } else {
+    console.log("no file chosen")
+  }
+}
+
+// TOOD: listen for peer join
+function handleReceiverJoin() {
+  new Receiver()
+}
+
+// create new receiver when a peer joins the socket.io room
+class Receiver {
+  peerConnection: RTCPeerConnection
+
+  constructor() {
+    this.peerConnection = new RTCPeerConnection(rtcConfig)
+    // TODO: save socket.io peer's id to send messages only to them
+    // TODO: create dataChannel & listen for 'open' event
+    // TODO: initialize listeners
+    void this.sendOffer()
+  }
+
+  async sendOffer() {
+    const offer = await this.peerConnection.createOffer()
+    void this.peerConnection.setLocalDescription(offer)
+    console.log(offer) // TODO: send offer to socket.io this peer
+  }
+
+  handleAnswer(answer: RTCSessionDescriptionInit) {
+    void this.peerConnection.setRemoteDescription(answer)
+  }
+}
