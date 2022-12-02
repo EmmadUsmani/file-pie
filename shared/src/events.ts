db09455dc1467bd70d1c718da8557bd630b5237b
Emmad Usmani <emmadausmani@gmail.com>
2022-12-02 07:52:16 -0800

    style(shared): :art: update event names to have a separate 'room' namespace

--- shared/src/events.ts
+++ shared/src/events.ts
@@ -1,21 +1,20 @@
 import { RoomID, ClientID } from "./types"
 
-// TODO: consider separate rooms namespace rather than handshake
 export const enum ServerEvent {
-  CreateRoom = "from_sender:handshake:create_room",
-  RoomCreated = "to_sender:handshake:room_created",
-  JoinRoom = "from_receiver:handshake:join_room",
-  RoomJoined = "to_receiver:handshake:room_joined",
-  RoomNotFound = "to_receiver:handshake:room_not_found",
-  ReceiverJoined = "to_sender:handshake:receiver_joined",
-  ReceiverLeft = "to_sender:handshake:receiver_left",
-  SenderLeft = "to_receiver:handshake:sender_left",
+  CreateRoom = "from_sender:room:create_room",
+  RoomCreated = "to_sender:room:room_created",
+  JoinRoom = "from_receiver:room:join_room",
+  RoomJoined = "to_receiver:room:room_joined",
+  RoomNotFound = "to_receiver:room:room_not_found",
+  ReceiverJoined = "to_sender:room:receiver_joined",
+  ReceiverLeft = "to_sender:room:receiver_left",
+  SenderLeft = "to_receiver:room:sender_left",
   SendOffer = "from_sender:handshake:send_offer",
   OfferSent = "to_receiver:handshake:offer_sent",
   SendAnswer = "from_receiver:handshake:send_answer",
   AnswerSent = "to_sender:handshake:answer_sent",
   SendIceCandidateToReceiver = "from_sender:handshake:send_ice_candidate_to_receiver",
-  IceCandidateSentFromSender = "to_receiver:handhsake:ice_candidate_sent_from_sender",
+  IceCandidateSentFromSender = "to_receiver:handshake:ice_candidate_sent_from_sender",
   SendIceCandidateToSender = "from_receiver:handshake:send_ice_candidate_to_sender",
   IceCandidateSentFromReceiver = "to_sender:handshake:ice_candidate_sent_from_receiver",
 }
